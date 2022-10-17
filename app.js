324f1e52b1d39512adf3afceab8a1f1f9900db9a
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-18 00:33:26 +0100

    saved

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ if (!fillColor.checked) {
 }
 
 const drawCircle = (e) => {
-    ctx.beginPath();r
+    ctx.beginPath();
     let radius = Math.sqrt(Math.pow((prevMouseX - e.offsetX), 2) + Math.pow((prevMouseY - e.offsetY), 2));
     ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI);
     fillColor.checked ? ctx.fill() : ctx.stroke(); 
