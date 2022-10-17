2556aed53e619f5a12bca38162322fa4e22b703f
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-17 15:23:06 +0100

    save file

---
+++ app.js
@@ -0,0 +1,35 @@
+
+const canvas =  document.getElementById("canvas");
+let colorBtn = document.querySelectorAll('.option');
+colo_picker = document.getElementById('color-picker')
+ctx = canvas.getContext("2d");
+
+selectedColor = "#000",
+brushWidth = 7;
+window.addEventListener("load", ()=>{
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+
+    canvaWidth = canvas.width;
+    canvaHeight = canvas.height;
+
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(0, 0, canvaWidth, canvaHeight );
+    ctx.fillStyle = selectedColor;
+})
+
+colorBtn = [...colorBtn]
+colorBtn.forEach(btn=>{
+    btn.addEventListener('click', ()=>{
+        eachBtn = document.querySelector('.selected')
+        eachBtn.classList.remove('selected')
+        btn.classList.add('selected');
+        selectedColor = window.getComputedStyle(btn).background;
+        console.log(selectedColor);
+    })
+})
+colo_picker.addEventListener('change', (e)=>{
+   let listTag = colo_picker.parentElement;
+   listTag.style.background = e.target.value;
+   listTag.click();
+})
